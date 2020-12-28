02583137abe3b935d6ca41f5739e1a4862f5b02d
MordecaiOhhh <ayan1999asd@gmail.com>
2020-12-28 19:15:18 +0600

    POPUP :: added with fixed positions

---
+++ js/dist/main.dev.js
@@ -0,0 +1,49 @@
+"use strict";
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function openModal($el) {
+  $(".on-front-desc.active").removeClass("active");
+
+  var _target = $el.attr('aria-label');
+
+  $(".modal_content[aria-target='" + _target + "']:not(.active)").addClass("active");
+}
+
+$(".btn-more-info").click(function () {
+  openModal($(this));
+});
+$(".btn-add-cart").click(function () {
+  openModal($(this));
+});
+$(".btn-more-info-6").click(function () {
+  openModal($(this));
+});
+$(".btn-add-cart-6").click(function () {
+  openModal($(this));
+});
+$(".btn-more-info-7").click(function () {
+  openModal($(this));
+});
+$(".btn-add-cart-7").click(function () {
+  openModal($(this));
+});
+var swiper = new Swiper('.swiper-container', _defineProperty({
+  direction: 'vertical',
+  slidesPerView: 1,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+}, "slidesPerView", 'auto'));
+$(".navToggle").click(function () {
+  $(this).toggleClass("open");
+  $("nav").toggleClass("open");
+});
+$(".btn-next").click(function () {
+  $(".swiper-pagination-bullet-active").next().click();
+});
+$(".btn-more-colse-modal").click(function () {
+  $(".on-front-desc.active").removeClass("active");
+});
